395c256299c46955dae5a6e16c74501129d2e87d
Cole-Nelms <colenelms12@gmail.com>
2021-11-27 20:37:42 -0500

    Added bash script for making iso file bootable from a media drive

---
+++ MakeLiveBoot.sh
@@ -0,0 +1,33 @@
+# This script is for making an
+# image file (.iso) bootable
+# through a media drive. It
+# requires root or sudo to
+# run. The first argument
+# accepts a path to the iso
+# file. The second Argument
+# accepts a path to the media
+# drive, (example: '/dev/sdb').
+# I have only tried it
+# on a usb drive with debian.
+# tweaks and changes may be
+# needed for other systems
+# and/or media drives.
+# Good luck!!
+
+#!/bin/sh
+
+# Unmount the media
+echo "umount $2*"
+umount $2*
+echo ""
+
+# Give media fat32 file system
+echo "mkfs.vfat $2"
+mkfs.vfat -I $2
+echo ""
+
+# Install .iso file to media
+echo "dd if=$1 of=$2 status=progress"
+dd if=$1 of=$2 status=progress
+echo ""
+
